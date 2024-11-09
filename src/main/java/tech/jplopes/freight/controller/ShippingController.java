e21e2c92131f44980850a320b3ee320ce07cd2e6
jplopes <joaolacaia96@gmail.com>
2024-11-08 21:30:39 -0300

    Service Class created

--- src/main/java/tech/jplopes/freight/controller/ShippingController.java
+++ src/main/java/tech/jplopes/freight/controller/ShippingController.java
@@ -1,21 +1,27 @@
 package tech.jplopes.freight.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tech.jplopes.freight.controller.dto.ShippingResponse;
+import tech.jplopes.freight.service.ShippingService;
 
 @RestController
 public class ShippingController {
+
+    @Autowired
+    private ShippingService shippingService;
 
     @GetMapping(value = "/shipping/calculate")
     public ResponseEntity<ShippingResponse> calculate(@RequestParam("type") String shippingType,
                                                       @RequestParam("distance") Double distance,
                                                       @RequestParam("weight") Double weight){
         //Calculate
+        var coast = shippingService.calculate(shippingType, distance, weight);
 
-        return ResponseEntity.ok(new ShippingResponse(0.0));
+        return ResponseEntity.ok(new ShippingResponse(coast));
     }
 
 
---
+++ src/main/java/tech/jplopes/freight/service/ShippingService.java
@@ -0,0 +1,18 @@
+package tech.jplopes.freight.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ShippingService {
+
+
+
+    public Double calculate(String shippingType,
+                            Double distance,
+                            Double weight){
+
+        return 0.0;
+    }
+
+
+}
